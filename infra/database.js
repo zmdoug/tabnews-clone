9badedf158d723705282860e2b93df76639b5cf2
Douglas Zardinello <zmdoug@gmail.com>
2025-01-12 01:35:23 +0000

    make database.js more structured

--- infra/database.js
+++ infra/database.js
@@ -1,18 +1,24 @@
-import { Client } from 'pg';
+import { Client } from "pg";
 
 async function query(queryObject) {
-    const client = new Client({
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
-        user: process.env.POSTGRES_USER,
-        database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD
-    });
-    await client.connect();
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+
+  try {
     const result = await client.query(queryObject);
-    await client.end();
     return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 export default {
-    query: query
+  query: query,
 };
